adf0b0601e31c59d426010e77060cc272acc3892
rileyeh <rhatchdev@gmail.com>
2021-07-26 17:01:53 -0700

    updated packages and some file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Budget from './Components/Budget/Budget';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <HashRouter>
+        <Switch>
+          <Route path='/' component={Login} exact />
+          <Route path='/my-budget' component={Budget} />
+        </Switch>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
